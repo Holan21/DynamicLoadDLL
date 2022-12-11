468b7cca19d194f27df8c17b1987d2ed8602dabb
Zaun14 <zaunmasters@gmail.com>
2022-12-12 00:07:27 +0200

    first commit

---
+++ DLL/DLL/DLL.cs
@@ -0,0 +1,15 @@
+﻿namespace DLL
+{
+    public class DLL
+    {
+        public void WriteString(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public string GetString() 
+        {
+            return "Test1";
+        }
+    }
+}
---
+++ DynamicLoad/DynamicLoad/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicLoad
+{
+    public class Program
+    {
+        private static string _path = @"lib";
+        public static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_path);
+            var _fullPath = dirInfo.FullName;
+
+            if (!Directory.Exists(_path)) Directory.CreateDirectory(_path);
+
+            string[] files = Directory.GetFiles(_fullPath + @"\" , "*.dll");
+
+            foreach (string file in files)
+            {
+                var loadDLL = Assembly.LoadFile(@$"{file}");
+                string[] strings = file.Split(@"\");
+                Console.WriteLine("Invoke From:" + strings[strings.Length -1]);
+ 
+                foreach (Type type in loadDLL.GetExportedTypes())
+                {
+                    var c = Activator.CreateInstance(type);
+
+                    var writeInConsole = type.GetMethod("WriteString");
+                    var getString = type.GetMethod("GetString");
+
+                    Console.Write("   Result method " + writeInConsole.Name + ":");
+                    writeInConsole.Invoke(c , new object[] { "Random Message"});
+
+                    Console.Write("   Result method " + getString.Name + ":");
+                    Console.WriteLine(getString.Invoke(c, null));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
